32f2036e9b37d068e835f64ce02c19810df42989
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-08-13 20:54:22 +0530

    change package name

--- src/main/java/com/ecom/user/UserServiceApplication.java
+++ src/main/java/com/ecom/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ecom.userservice;
+package com.ecom.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ecom/user/config/AppConfig.java
+++ src/main/java/com/ecom/user/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.ecom.userservice.config;
+package com.ecom.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
