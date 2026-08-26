f324d0be289abd3adcd1aa5f55783819981008c8
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-08-27 00:33:30 +0530

    expose auth controller to all cross origins

--- src/main/java/com/ecom/user/controller/AuthController.java
+++ src/main/java/com/ecom/user/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.ecom.user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
 
 	private final AuthService authService;
