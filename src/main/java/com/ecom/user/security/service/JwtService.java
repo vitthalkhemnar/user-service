d2e4278c2b4c79b0dc5c186ced56256f02e0dcaa
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-09-25 04:53:01 +0530

    implemented api gateway

--- src/main/java/com/ecom/user/security/service/JwtService.java
+++ src/main/java/com/ecom/user/security/service/JwtService.java
@@ -1,17 +1,12 @@
 package com.ecom.user.security.service;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
-import org.jspecify.annotations.Nullable;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
